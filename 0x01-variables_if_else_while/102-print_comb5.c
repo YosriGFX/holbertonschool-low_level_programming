b69747a67bc676ac63960ebc0516a31bced3f276
yosrigfx <yosrigfx@gmail.com>
2019-09-19 13:21:05 +0000

    first person to finish all

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * print_alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+int m;
+int t;
+int e;
+for (n = 48; n <= 57; n++)
+for (m = 48; m <= 57; m++)
+for (t = 48; t <= 57; t++)
+for (e = 48; e <= 57; e++)
+if (m < e)
+{
+putchar(n);
+putchar(m);
+putchar(' ');
+putchar(t);
+putchar(e);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
