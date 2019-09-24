5ee9145e4679fdc1097e2fc59100df9893ddb011
yosrigfx <yosrigfx@gmail.com>
2019-09-24 18:07:34 +0000

    easy way

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-int a, b, c, d;
+unsigned long int a, b, c, d;
 b = 1;
 c = 0;
-for (a = 0; a <= 50; a++)
+for (a = 0; a < 50; a++)
 {
 d = c + b;
 c = b;
 b = d;
-printf("%i", d);
-if (a != 50)
+printf("%lu",d);
+if (a != 49)
 printf(", ");
 }
 printf("\n");
