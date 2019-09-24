74e173cbd4391c333ae91aba282a9b5a34081e21
yosrigfx <yosrigfx@gmail.com>
2019-09-24 15:17:11 +0000

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - a  function that adds two integers and returns the result.
+ * @a: integer
+ * @b: integer
+ * Return: rezult
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
