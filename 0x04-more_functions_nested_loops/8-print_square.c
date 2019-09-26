d38286b7d8e2f7f2206f628cb58038104932788a
yosrigfx <yosrigfx@gmail.com>
2019-09-26 10:29:25 +0000

    Dsquared2

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_square - function that prints a square, followed by a new line.
+ * @size: is the size of the square
+ * Return: void
+ */
+void print_square(int size)
+{
+int a, i;
+if (size <= 0)
+putchar('\n');
+for (a = 0; a < size; a++)
+{
+for (i = 0; i < size; i++)
+putchar('#');
+putchar('\n');
+}
+}
