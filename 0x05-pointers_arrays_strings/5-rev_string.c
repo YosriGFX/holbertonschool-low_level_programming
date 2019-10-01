6919930c5e1ace0cba16d3ccb3a05ed093a55c1e
yosrigfx <yosrigfx@gmail.com>
2019-10-01 17:02:53 +0000

    betty

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,7 +7,7 @@
 void rev_string(char *s)
 {
 int a = 0, b = 0;
-char *c = s;
+char c;
 while (s[a])
 a++;
 while (b <= a / 2)
@@ -16,4 +16,5 @@ c = s[a - b - 1];
 s[a - b - 1] = s[b];
 s[b] = c;
 b++;
+}
 }
