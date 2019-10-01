84da65c642064dbc88427c36fc5f053a7ae88c60
yosrigfx <yosrigfx@gmail.com>
2019-10-01 20:19:49 +0000

    commit all

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,12 +6,13 @@
  */
 void puts_half(char *str)
 {
-int a = 0, b;
-while (str[a])
+int a = 0, b, i;
+while (str[a] != '\0')
 a++;
+i = a;
 if (a % 2 != 0)
-a++;
-for (b = a / 2; b < a; b++)
+i++;
+for (b = i / 2; b <= a; b++)
 _putchar(str[b]);
 _putchar('\n');
 }
