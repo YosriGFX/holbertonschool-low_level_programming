f28327713a326aee117f1f35012643abc6efa148
YosriGFX <1111@holbertonschool.com>
2019-10-27 15:42:21 +0000

    sum ➕➕

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - function that return sum
+ * @n: unsigned int
+ * @...: int
+ * Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+if (n == 0)
+return (0);
+unsigned int a = 0;
+int t = 0;
+va_list s;
+va_start(s, n);
+while (a < n)
+{
+t += va_arg(s, int);
+a++;
+}
+va_end(s);
+return (t);
+}
