1fc5509b3aa87522981486acdc6cdc216bd8061d
Yosri Ghorbel <yosrig1997@gmail.com>
2019-11-05 21:13:06 +0000

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,13 +7,13 @@
  */
 void free_listint2(listint_t **head)
 {
-listint_t *nn;
+listint_t *next;
 if (head == NULL)
 return;
 while (*head != NULL)
 {
-nn = (*head)->nn;
+next = (*head)->next;
 free(*head);
-*head = nn;
+*head = next;
 }
 }
