7a755e63532e305ef3851e67f010ef1d8c2260e4
Yosri Ghorbel <yosrig1997@gmail.com>
2019-11-05 21:03:29 +0000

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint -  prints all.
+ * @h: list to print
+ * Return: number
+ */
+size_t print_listint(const listint_t *h)
+{
+size_t a = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+a++;
+h = h->nn;
+}
+return (a);
+}
