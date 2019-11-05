4086982f40e78042b4d048e8418a242a7afded4f
Yosri Ghorbel <yosrig1997@gmail.com>
2019-11-05 21:03:29 +0000

    8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * sum_listint - returns the syl
+ * @head: pointer
+ * Return: add
+ */
+int sum_listint(listint_t *head)
+{
+int add = 0;
+while (head != NULL)
+{
+add += head->n;
+head = head->nn;
+}
+return (add);
+}
