29328ff8cac918a8240b8ef0826f5f809f4e1119
YosriGFX <yosrig1997@gmail.com>
2019-12-14 11:14:21 -0500

    sorry

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,51 +1,48 @@
 #include "lists.h"
 /**
- * insert_dnodeint_at_index - insert node at index
- * @h: pointer to list
- * @idx: index
- * @n: int
- * Return: the address of the new node, or NULL
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: address of pointer to head node
+ * @idx: index of list to insert node
+ * @n: data for new node
+ * Return: address of new node, or NULL if it failed or idx doesn't exist
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-unsigned int count = 0, a;
-if (*h != NULL)
-{
-dlistint_t *temp = *h;
-while (temp)
-{
-temp = temp->next;
-count++;
-}
-if (idx == 0)
-{
-add_dnodeint(h, n);
-}
-else if (idx == count)
-{
-add_dnodeint_end(h, n);
-}
-else if (idx < count)
-{
-dlistint_t *temp = *h;
-dlistint_t *new = malloc(sizeof(dlistint_t));
+dlistint_t *new, *temp = *h;
+if (h == NULL)
+return (NULL);
+new = malloc(sizeof(dlistint_t));
 if (new == NULL)
 return (NULL);
-for (a = 0; temp != NULL; a++)
-{
-if (idx == a + 1)
-{
 new->n = n;
-new->next = temp->next;
-new->prev = temp;
-if (temp->next != NULL)
-temp->next->prev = new;
-temp->next = new;
+if (idx == 0 && *h == NULL)
+{
+new->prev = NULL;
+new->next = NULL;
+*h = new;
 return (new);
 }
-temp = temp->next;
+if (idx == 0 && temp != NULL)
+{
+new->next = temp, new->prev = NULL;
+temp->prev = new, *h = new;
+return (new);
 }
+while (temp != NULL && idx > 1)
+temp = temp->next, idx--;
+if (temp != NULL)
+{
+new->next = temp->next;
+new->prev = temp;
+if (temp->next == NULL)
+temp->next = new;
+else
+{
+temp->next->prev = new;
+temp->next = new;
 }
+return (new);
 }
+free(new);
 return (NULL);
 }
