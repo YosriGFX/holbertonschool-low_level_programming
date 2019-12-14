8c0142f2abe84ffaa9d936f65aad4bfefb3343e8
YosriGFX <yosrig1997@gmail.com>
2019-12-14 18:53:59 -0500

    please

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,41 +7,45 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-unsigned int a = 0, b = 0;
-dlistint_t *delete;
-dlistint_t *counter;
-counter = *head;
-if (counter == NULL)
-	return (-1);
-while (counter != NULL)
+if ((*head)->next)
 {
-	counter = counter->next;
-	a++;
-}
-free(counter);
-delete = *head;
-
-if (index > a)
+	unsigned int a = 0, b = 0;
+	dlistint_t *delete;
+	dlistint_t *counter;
+	counter = *head;
+	while (counter != NULL)
+	{
+		counter = counter->next;
+		a++;
+	}
+	delete = *head;
+	if (index >= a)
+		return (-1);
+	else if ((index == 0) && (delete->prev == 0))
+	{
+		if (delete->next == NULL)
+		{
+			return (-1);
+		}
+		else
+			delete = delete->next;
+		delete->prev = NULL;
+		*head = delete;
+		return (1);
+	}
+	else if (index < a)
+	{
+		for (b = 0; b < index - 1; b++)
+			delete = delete->next;
+		delete->next = delete->next->next;
+		delete->next->prev = delete;
+		return (1);
+	}
 	return (-1);
+}
 else
 {
-if ((index == 0) && (delete->prev == NULL) && (delete->next != NULL))
-{
-	delete = delete->next;
-	delete->prev = NULL;
-	*head = delete;
-	free(delete);
-	return (1);
-}
-else if (delete->next != NULL)
-{
-	for (b = 0; b < index - 1; b++)
-		delete = delete->next;
-	delete->next = delete->next->next;
-	delete->next->prev = delete;
-	return (1);
-}
-*head = NULL;
-return (1);
+	head = NULL;
+	return (-1);
 }
 }
