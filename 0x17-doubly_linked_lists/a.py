d5a13ab4a7b8548b615ddde8a6c06e54ddc125cb
YosriGFX <yosrig1997@gmail.com>
2019-12-14 19:22:46 -0500

    me

--- 0x17-doubly_linked_lists/a.py
+++ 0x17-doubly_linked_lists/a.py
@@ -1,16 +1,10 @@
-# uncompyle6 version 3.6.0
-# Python bytecode 3.4 (3310)
-# Decompiled from: Python 3.7.3 (default, Nov 15 2019, 04:04:52) 
-# [Clang 11.0.0 (clang-1100.0.33.16)]
-# Embedded file name: 100-crackme.py
-# Compiled at: 2017-01-05 19:44:37
-# Size of source mod 2**32: 195 bytes
-pwd = input('Password: ')
-ok = 'Zen of Python'
-ok = ok + ' C'
-ok = ok[1:3] + ok[3] + ok[14] + ok[3] + ok[7:9] + ok[:6][::-1]
-if pwd == ok:
-    print('OK')
-else:
-    print('KO')
-# okay decompiling a.pyc
+#!/usr/bin/python3
+n = 0
+for a in range(9999, 100, -1):
+    for b in range(a, 100, -1):
+        x = a * b
+        if x > n:
+            s = str(a * b)
+            if s == s[::-1]:
+                n = a * b
+print("{} = {} x {}".format(n, a, b))
