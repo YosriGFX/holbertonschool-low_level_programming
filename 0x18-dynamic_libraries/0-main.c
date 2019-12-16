0438d799526a1ca9431e96fdc3b6f805d9181366
YosriGFX <YosriG1997@gmail.com>
2019-12-16 17:49:54 +0000

    Not sure

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,14 @@
+nclude "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    printf("%d\n", _strlen("Holberton"));
+    return (EXIT_SUCCESS);
+}
+
