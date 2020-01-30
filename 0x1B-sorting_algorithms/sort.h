b0d39bea51db8a4e4bdf8a6956e1d89245e3090e
YosriGFX <YosriG1997@gmail.com>
2020-01-30 20:37:54 +0100

    0-bubble_sort.c

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -1,5 +1,7 @@
 #ifndef _SORT_H_
 #define _SORT_H_
+#include <stddef.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -16,5 +18,6 @@ typedef struct listint_s
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
 
-#endif _SORT_H_
+#endif
